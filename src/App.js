2ddcefa8c2232496060ce08dac83ec34ab986dd3
CalvinC44 <calel44jke@gmail.com>
2023-09-28 15:52:52 +0200

    fix

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+// import GravitySvgComponent from "./GravitySvgComponent";
+
+function App() {
+	return (
+		<div className="App">
+			<div className="App-header">
+				<h1>Calvin CHIFFOT</h1>
+			</div>
+			<h1 className="App-header">test</h1>
+		</div>
+	);
+}
+
+export default App;
